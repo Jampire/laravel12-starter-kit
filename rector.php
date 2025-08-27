e0c6c2d1f46c0707915edddfcd6d64bb0cc73108
Dzianis Kotau <me@dzianiskotau.com>
2025-08-27 23:40:43 +0200

    Initial commit

---
+++ rector.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use RectorLaravel\Set\LaravelSetProvider;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/app',
+        __DIR__ . '/bootstrap',
+        __DIR__ . '/config',
+        __DIR__ . '/public',
+        __DIR__ . '/resources',
+        __DIR__ . '/routes',
+        __DIR__ . '/tests',
+    ])
+//    ->withSkip([
+//        AddOverrideAttributeToOverriddenMethodsRector::class,
+//    ])
+    ->withPhpSets()
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+        strictBooleans: true,
+    )
+    ->withSetProviders(LaravelSetProvider::class)
+    ->withComposerBased(laravel: true);
